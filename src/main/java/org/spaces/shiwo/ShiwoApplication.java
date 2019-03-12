b1955b6422cf7188a28fd429bb236d575fc41696
aitlp <aitlpmw@gmail.com>
2019-03-12 15:13:22 +0800

    init project

---
+++ src/main/java/org/spaces/shiwo/ShiwoApplication.java
@@ -0,0 +1,13 @@
+package org.spaces.shiwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiwoApplication.class, args);
+	}
+
+}
