d13974ad8d884db4b331cdf1d95187d25e1a0793
aitlp <aitlpmw@gmail.com>
2019-04-19 12:40:48 +0800

    增加swagger2支持；

--- src/main/java/com/aitlp/ShiwoApplication.java
+++ src/main/java/com/aitlp/ShiwoApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan({"com.aitlp.shi.dao"})
-
+@EnableSwagger2
 public class ShiwoApplication {
 
     public static void main(String[] args) {
